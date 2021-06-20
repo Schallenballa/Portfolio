b27c6ca302fbe7ec27ebbc9647108708a4a2eedb
Zachary Schallenberger <zapps98@gmail.com>
2021-06-20 14:16:54 -0500

    Volunteering/  Added dynamic footer
    Added volunteering section to homepage
    Footer now dynamically fetches the latest commit to the repository

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  const returnVar =
+  fetch(
+      "https://api.github.com/repos/Schallenballa/Portfolio/branches/main"
+    )
+    .then(response => {
+      response.json().then(json => {
+        document.getElementById('fetchDate').innerHTML = "© Last Updated - " + json.commit.commit.author.date.slice(0,10) + " @ " + json.commit.commit.author.date.slice(11,16);
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
