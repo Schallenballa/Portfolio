d6af0b185c815b7f6e569a51bf4c5804d9918035
Zachary Schallenberger <zapps98@gmail.com>
2022-08-08 20:13:52 -0400

    Performance Upgrades
    Sanitized string, reduced image sizes, changed image types, updated libraries,

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ window.onload = function() {
     )
     .then(response => {
       response.json().then(json => {
-        document.getElementById('fetchDate').innerHTML = "Zachary Schallenberger © Last Updated - " + json.commit.commit.author.date.slice(0,10) + " @ " + json.commit.commit.author.date.slice(11,16);
+        sanitizeHTML("Zachary Schallenberger © Last Updated - " + json.commit.commit.author.date.slice(0,10) + " @ " + json.commit.commit.author.date.slice(11,16));
       });
     })
     .catch(error => {
       console.log(error);
     });
 }
+
+/*!
+ * Sanitize and encode all HTML in a user-submitted string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {String} str  The user-submitted string
+ * @return {String} str  The sanitized string
+ */
+var sanitizeHTML = function (str) {
+	var temp = document.getElementById('fetchDate');
+	temp.textContent = str;
+	return temp.innerHTML;
+};
