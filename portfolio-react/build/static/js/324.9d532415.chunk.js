f5fa230c4f54da0556ffd9c52d244c993c9831b0
Zachary Schallenberger <zachietaylor@msn.com>
2025-01-21 17:43:05 -0500

    Optimizations

---
+++ portfolio-react/build/static/js/324.9d532415.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio_react=self.webpackChunkportfolio_react||[]).push([[324],{324:function(e,r,t){t.r(r);t(43);var a=t(579);r.default=({src:e,width:r,height:t})=>(0,a.jsx)("iframe",{src:e,width:r,height:t,frameBorder:"0",allow:"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture",loading:"lazy",style:{padding:"20px 0"},title:"Standard Track Player"})}}]);
+//# sourceMappingURL=324.9d532415.chunk.js.map
